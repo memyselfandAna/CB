e66a8f83c13f45a970c4efc521715fe6ac4a334e
memyselfandAna <98179942+memyselfandAna@users.noreply.github.com>
2022-11-11 20:06:26 +0200

    Blog Form

--- OurSite/OurSite/urls.py
+++ OurSite/OurSite/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+
 from personal.views import (
     home_screen_view,
 )
@@ -26,7 +27,8 @@ from account.views import (
     registration_view,
     logout_view,
     login_view,
-    account_view
+    account_view,
+    must_authenticate_view,
 )
 
 urlpatterns = [
@@ -37,8 +39,13 @@ urlpatterns = [
     path('logout/', logout_view, name="logout"),
     path('login/', login_view, name="login"),
     path('account/', account_view, name="account"),
+    path('must_authenticate/', must_authenticate_view, name="must_authenticate"),
 
-        # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
+    # home_screen_view este numele din personal/views.py
+    # name="home" este optional
+
+    # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
+
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
         name='password_change_done'),
 
@@ -46,20 +53,18 @@ urlpatterns = [
         name='password_change'),
 
     path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
-         name='password_reset_done'),
+     name='password_reset_done'),
 
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
-        name='password_reset_complete'),   
-]
+     name='password_reset_complete'),
 
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# home_screen_view este numele din personal/views.py
-# name="home" este optional
 
--- OurSite/account/views.py
+++ OurSite/account/views.py
@@ -75,3 +75,6 @@ def account_view(request):
 
     context['account_form'] = form
     return render(request, 'account/account.html', context)
+
+def must_authenticate_view(request):
+    return render(request, 'account/must_authenticate.html', {})
