6b72940278b41476d11222b7189a3bdc46566f67
memyselfandAna <98179942+memyselfandAna@users.noreply.github.com>
2022-11-14 15:53:35 +0200

    Edit, Upadate blogposts

--- OurSite/blog/views.py
+++ OurSite/blog/views.py
@@ -7,29 +7,58 @@ from account.models import Account
 
 def create_blog_view(request):
 
-    context = {}
+	context = {}
 
-    user = request.user
-    if not user.is_authenticated:
-        return redirect('must_authenticate')
+	user = request.user
+	if not user.is_authenticated:
+		return redirect('must_authenticate')
 
-    form = CreateBlogPostForm(request.POST or None, request.FILES or None)
-    if form.is_valid():
-        obj = form.save(commit=False)
-        author = Account.objects.filter(email=user.email).first()
-        obj.author = author
-        obj.save()
-        form = CreateBlogPostForm
+	form = CreateBlogPostForm(request.POST or None, request.FILES or None)
+	if form.is_valid():
+		obj = form.save(commit=False)
+		author = Account.objects.filter(email=request.user.email).first()
+		obj.author = author
+		obj.save()
+		form = CreateBlogPostForm()
 
-    context['form'] = form
+	context['form'] = form
 
-    return render(request, "blog/create_blog.html", {})
+	return render(request, 'blog/create_blog.html', context)
+
+
 
 def detail_blog_view(request, slug):
 
-    context = {}
+	context = {}
+	blog_post = get_object_or_404(BlogPost, slug=slug)
+	context['blog_post'] = blog_post
 
-    blog_post = get_object_or_404(BlogPost, slug=slug)
-    context['blog_post'] = blog_post
+	return render(request, 'blog/detail_blog.html', context)
+    
 
-    return render(request, 'blog/detail_blog.html', context)
+def edit_blog_view(request, slug):
+	
+	context = {}
+	user = request.user
+	if not user.is_authenticated:
+		return redirect('must_authenticate')
+
+	blog_post = get_object_or_404(BlogPost, slug=slug)
+	if request.POST:
+		form = CreateBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
+		if form.is_valid():
+			obj = form.save(commit=False)
+			obj.save()
+			context['success_message'] = "Updated"
+			blog_post = obj
+	
+	form = CreateBlogPostForm(
+			initial={
+					"title": blog_post.title, 
+					"body": blog_post.body,
+					"image": blog_post.image,
+				}
+			)
+	context['form'] = form
+
+	return render(request, 'blog/edit_blog.html', context)
