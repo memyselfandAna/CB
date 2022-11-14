e12ddc5f9a422dc8d8f81665480e705f2c81248e
memyselfandAna <98179942+memyselfandAna@users.noreply.github.com>
2022-11-14 21:56:20 +0200

    Bara de cautare

--- OurSite/blog/views.py
+++ OurSite/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.db.models import Q
 from blog.models import BlogPost
-from blog.forms import CreateBlogPostForm
+from blog.forms import CreateBlogPostForm, UpdateBlogPostForm
 from account.models import Account
 
 
@@ -26,7 +26,6 @@ def create_blog_view(request):
 	return render(request, 'blog/create_blog.html', context)
 
 
-
 def detail_blog_view(request, slug):
 
 	context = {}
@@ -34,7 +33,7 @@ def detail_blog_view(request, slug):
 	context['blog_post'] = blog_post
 
 	return render(request, 'blog/detail_blog.html', context)
-    
+
 
 def edit_blog_view(request, slug):
 	
@@ -45,14 +44,14 @@ def edit_blog_view(request, slug):
 
 	blog_post = get_object_or_404(BlogPost, slug=slug)
 	if request.POST:
-		form = CreateBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
+		form = UpdateBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
 		if form.is_valid():
 			obj = form.save(commit=False)
 			obj.save()
 			context['success_message'] = "Updated"
 			blog_post = obj
 	
-	form = CreateBlogPostForm(
+	form = UpdateBlogPostForm(
 			initial={
 					"title": blog_post.title, 
 					"body": blog_post.body,
@@ -62,3 +61,18 @@ def edit_blog_view(request, slug):
 	context['form'] = form
 
 	return render(request, 'blog/edit_blog.html', context)
+
+
+def get_blog_queryset(query=None):
+	queryset = []
+	queries = query.split(" ")
+	for q in queries:
+		posts = BlogPost.objects.filter(
+			Q(title__icontains=q)|
+			Q(body__icontains=q)
+			).distinct()
+
+		for post in posts:
+			queryset.append(post)
+
+	return list(set(queryset))
